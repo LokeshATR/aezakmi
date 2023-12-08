73bf6cf87592b244e023571fd356e19344c885bd
AryanSangwan <aryan.sangwan@antiersolutions.com>
2023-12-09 02:09:23 +0530

    scripts, CryptoCab

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -1,32 +1,23 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+const { ethers, upgrades } = require("hardhat");
 const hre = require("hardhat");
-
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+   
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  driver = await ethers.getContractFactory("Driver");
+  console.log("1");
+  driver = await driver.deploy("0xD8C99a04eE4611bfbe3822606242bCcD59cAB160");
+  console.log("2");
+  console.log("driver address: ",driver.target);
 
-  await lock.waitForDeployment();
+    sleep(5000)
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
